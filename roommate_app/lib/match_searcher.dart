7b74aa0fe82486df056df92f6f4feac24ddec6f3
Jack Sheehan <jaxanimations@gmail.com>
2019-10-06 11:51:02 -0500

    Created MatchSearcher class

---
+++ roommate_app/lib/match_searcher.dart
@@ -0,0 +1,10 @@
+//This class is used to search for matches given a user's gender and location preferences
+
+import "profile.dart";
+
+class MatchSearcher {
+  int radius;
+  Profile userProfile;
+
+  MatchSearcher();
+}
--- scripts/city_location_scraper.py
+++ scripts/city_location_scraper.py
@@ -2,16 +2,36 @@ import bs4 as bs
 import urllib.request
 import sys
 
-TARGET_CITY_INDEX = 0
-TARGET_STATE_INDEX = 1
+TARGET_CITY_INDEX = 1
+TARGET_STATE_INDEX = 2
 
-target_city = sys.argv[TARGET_CITY_INDEX]   #The city to scrape the coordinates of will be passed as the first argument
-target_state = sys.argv[TARGET_STATE_INDEX] #The state to scrape the coordinates of will be passed as the second argument
+target_city = sys.argv[TARGET_CITY_INDEX]               #The city to scrape the coordinates of will be passed as the first argument
+target_state = sys.argv[TARGET_STATE_INDEX]             #The state to scrape the coordinates of will be passed as the second argument
 
-search_url = "https://www.wikidata.org/w/index.php?search=&search=" + target_city + "+" + target_state  #The URL that will search Wiki Data for location information
+SEARCH_RESULT_CLASS_NAME = "mw-search-result-heading"   #The name of the class of the div where the Wikidata search results are stored
 
-#Opening and reading the page source at search_url
-source_page = urllib.request.urlopen(search_url).read()
+#Create a URL that will point to the Wikidata search results for the city and state passed into the command line arguments
+search_url = "https://www.wikidata.org/w/index.php?search=&search=" + target_city + "+" + target_state
 
-#Creating a soup object to parse the HTML from source_page
-search_page_soup = bs.BeautifulSoup(source, "lxml")
+#DEBUG
+print("City: " + target_city)
+print("State: " + target_state)
+#DEBUG
+
+#Open and read the page source of search_url
+search_page_source = urllib.request.urlopen(search_url).read()
+
+#Create a soup object to parse the HTML from search_page_source
+search_page_soup = bs.BeautifulSoup(search_page_source, "lxml")
+
+#Get the href of the link of the top of the Wikidata results page
+top_result_link = search_page_soup.find("div", class_ = SEARCH_RESULT_CLASS_NAME).find("a").get("href")
+
+#Create a URL that points to the WikiData page for the city that's been passed into arguments
+information_page_url = "https://www.wikidata.org/" + top_result_link
+
+#Open and read the page soure of information_page_url
+information_page_source = urllib.request.urlopen(information_page_url, "lxml")
+
+print()
+
