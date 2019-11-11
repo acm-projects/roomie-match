7779bc10ce28734ee7035ec10db7a504181343cc
vidhimehta0524 <vidhimehta0524@gmail.com>
2019-11-11 08:55:48 -0600

    Image-picker

--- roommate_app/lib/screens/profile_creation_screen.dart
+++ roommate_app/lib/screens/profile_creation_screen.dart
@@ -5,6 +5,11 @@ import 'package:roommate_app/field_enforcer.dart';
 import 'package:roommate_app/screens/aboutme.dart';
 import '../user_info.dart';
 import 'signup.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:roommate_app/field_enforcer.dart';
+import 'package:path/path.dart';
 
 class ProfileCreationScreen extends StatefulWidget {
   String _selectedGender = kGENDER_MALE;  //Holds the current gender selection; initialzed to the default gender
@@ -14,6 +19,7 @@ class ProfileCreationScreen extends StatefulWidget {
 }
 
 class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
+  File _image;
   TextEditingController firstNameTextController = TextEditingController();
   TextEditingController lastNameTextController = TextEditingController();
   TextEditingController ageTextController = TextEditingController();
@@ -21,6 +27,62 @@ class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
   FocusNode lastNameFieldFocusNode = new FocusNode();
   FocusNode ageFieldFocusNode = new FocusNode();
 
+  Future getImageFromGallery(BuildContext context) async{
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState((){
+      _image = image;
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future getImageFromCamera(BuildContext context) async{
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState((){
+      _image = image;
+    });
+    Navigator.of(context).pop();
+
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context){
+    return showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("Make a choice:"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text("Gallery"),
+                onTap: (){
+                  getImageFromGallery(context);
+                },
+              ),
+              Padding(padding: EdgeInsets.all(8.0),),
+              GestureDetector(
+                child: Text("Camera"),
+                onTap: (){
+                  getImageFromCamera(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Future uploadPic(BuildContext context) async{
+      String fileName = basename(_image.path);
+      StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName);
+      StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+      StorageTaskSnapshot takeSnapshot = await uploadTask.onComplete;
+      setState(() {
+        print("Profile picture uploaded");
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text("Profile picture uploaded")));
+      });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,18 +107,38 @@ class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              //Profile image selection button
-              FlatButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 100),
-                  child: Icon(
-                  Icons.add_a_photo,
-                  color: Colors.black54,
-                  size: 100,
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.grey,
+                    child: ClipOval(
+                      child: SizedBox(
+                        width: 180,
+                        height : 180.0,
+                        child: (_image != null)? Image.file(_image, fit: BoxFit.fill):
+                        Image.network( "https://i0.wp.com/acaweb.org/wp-content/uploads/2018/12/profile-placeholder.png?fit=300%2C300&ssl=1",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    )
                   ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(top: 60.0),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.add_a_photo,
+                      size: 30.0,
+                    ),
+                    onPressed: (){ _showChoiceDialog(context);},
+                  ),
+                )
+              ],
+          ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 40.0),
                 child: Container(
@@ -168,8 +250,9 @@ class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
                     UserInformation.gender = widget._selectedGender;
                     UserInformation.age = int.parse(ageTextController.text);
 
+                    uploadPic(context);
                     //Route to about me page
-                    Navigator.push(
+                   Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => AboutMeScreen())
                     );
